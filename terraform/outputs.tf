36c3274ac079e27cd7b85f26d4e85081c264650c
Andy Neillans <andy@neillans.co.uk>
2024-10-08 18:37:02 +0100

    Add default hostname

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,7 @@ output "app_service_plan_id" {
 output "identity_id" {
     value = data.azurerm_linux_function_app.function_wrapper.identity[0].principal_id
 }
+
+output "default_hostname" {
+    value = data.azurerm_linux_function_app.function_wrapper.default_hostname
+}
