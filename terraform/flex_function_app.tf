738ac04781aa4d1fb03b1b0e92ca2f61d46ee711
Andy Neillans <6953049+aneillans@users.noreply.github.com>
2024-10-11 20:02:08 +0100

    Auth (#1)

--- terraform/flex_function_app.tf
+++ terraform/flex_function_app.tf
@@ -49,3 +49,52 @@ resource "azapi_resource" "linux_flex_function_app" {
   })
   depends_on = [azapi_resource.server_farm_plan, azurerm_storage_account.storage_account]
 }
+
+resource "azapi_resource" "linux_flex_function_app" {
+  type                      = "Microsoft.Web/sites/config@2023-12-01"
+  schema_validation_enabled = false
+  location                  = var.location
+  name                      = "authsettingsV2"
+  parent_id                 = azapi_resource.linux_flex_function_app.id
+  body = jsonencode({
+    kind = "string",
+    properties = {
+      globalValidation = {
+        redirectToProvider = var.auth_redirect_to_provider,
+        requireAuthentication = var.auth_require_authentication,
+        unauthentedClientAction = var.auth_unauthentication_action
+      },
+      httpSettings = {
+        forwardProxy = {
+          convention = var.auth_forward_proxy_convention
+        },
+        requireHttps = var.auth_require_https,
+        routes = {
+          apiPrefix = var.auth_http_route_api_prefix
+        }
+      },
+      platform = {
+        enabled = var.auth_enabled,
+        runtimeVersion = var.auth_runtime_version
+      },
+      identityProviders = {
+        "OpenIDAuth" = {
+          enabled = "true",
+          registration = {
+            clientId = var.auth_client_id,
+            clientCredential = {
+              clientSecretSettingName = var.auth_client_secret_setting_name
+            },
+            openIdConnectConfiguration = {
+              authorizationEndpoint = var.auth_openid_auth_endpoint,
+              tokenEndpoint = var.auth_openid_token_endpoint,
+              issuer = var.auth_openid_issuer,
+              certificationUri = var.auth_openid_certification_uri,
+              wellKnownOpenIdConfiguration = var.auth_openid_well_known_configuration
+            }
+          }
+        }
+      }
+    }
+  })
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -52,11 +52,74 @@ variable "app_settings" {
 }
 
 variable "cors_allowed_origins" {
-  type = list(string)
+  type    = list(string)
   default = ["*"]
 }
 
 variable "cors_support_credentials" {
   type    = bool
   default = false
+}
+
+variable "auth_redirect_to_provider" {
+  type = string
+}
+
+variable "auth_require_authentication" {
+  type = bool
+}
+
+variable "auth_unauthentication_action" {
+  type = string
+}
+
+variable "auth_forward_proxy_convention" {
+  type = string
+}
+
+variable "auth_require_https" {
+  type = bool
+}
+
+variable "auth_http_route_api_prefix" {
+  type    = string
+  default = "/api"
+}
+
+variable "auth_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "auth_runtime_version" {
+  type    = string
+  default = "~1"
+}
+
+variable "auth_client_id" {
+  type = string
+}
+
+variable "auth_client_secret_setting_name" {
+  type = string
+}
+
+variable "auth_openid_auth_endpoint" {
+  type = string
+}
+
+variable "auth_openid_token_endpoint" {
+  type = string
+}
+
+variable "auth_openid_issuer" {
+  type = string
+}
+
+variable "auth_openid_certification_uri" {
+  type = string
+}
+
+variable "auth_openid_well_known_configuration" {
+  type = string
 }
